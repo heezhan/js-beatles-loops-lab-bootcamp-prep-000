b36041a8d21946b5231e12bdc1df2c9edfac7e1d
Heejae Han <hhan1194@gmail.com>
2019-07-31 02:46:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < musicians.length; i++) {
+      array.push(musicians[i] + ' plays ' + instruments[i]);
+    }
+  return array;
+}
+
+function johnLennonFacts (facts) {
+  var i = 0;
+  while (i < facts.length) {
+    facts[i] = facts[i] + '!!!';
+    i++
+  }
+  return facts;
+}
+
+function iLoveTheBeatles (number) {
+  var array = [];
+  var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+ 
+do {
+  console.log("doo-bee-doo-bee-doo");
+} while (incrementVariable() < 5);
+}
